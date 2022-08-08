5488f56030f1e3eb56fed30059ab488dc53a9876
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-08 02:19:29 -0500

    started project cards
    getting response from DB and from loremflickr...need to create product cards

--- components/productCards/productCards.tsx
+++ components/productCards/productCards.tsx
@@ -2,16 +2,58 @@ import Card from "@mui/material/Card";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 
+type products = [
+  {
+    category: { categoryName: string; id: number };
+    categoryId: number;
+    id: number;
+    price: number;
+    productName: string;
+    stock: number;
+    tags: [
+      {
+        id: number;
+        tagName: string;
+      }
+    ];
+  }
+];
+
 export const ProductCards: React.FC = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<products>();
+  const [productImg, setImg] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost:3001/api/products").then((res) => {
       setProducts(res.data);
-      console.info(res.data);
+      console.info(products);
+      // getImages();
     });
   }, []);
 
-  console.info('state =', products)
-  return <section></section>;
+  // return the JPG
+  axios.get(`https://loremflickr.com/320/320/plaint-shirt`).then((res) => {
+    console.info(res.data);
+  });
+
+  // function getImages() {
+  //   products.forEach((e, i: number) => {
+  //     axios
+  //       .get(`https://loremflickr.com/320/320/${products[i].productName}`)
+  //       .then((res) => {
+  //         return setImg(res.data);
+  //       });
+  //   });
+  // };
+
+  console.info(products);
+  console.log(products?.[0].productName);
+
+  // console log the product name from the product array
+
+  return (
+    <section>
+      <img src={productImg[0]} />
+    </section>
+  );
 };
